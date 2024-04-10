d67573ee57498d2d3a8554b6b0993fb658b22d30
WillamNyunt <williamnyunt@outlook.com>
2024-04-10 13:27:51 +0100

    Added Cart.jsx MealList.jsx to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Cart from "./components/cart";
+import MealList from "./components/MealList";
+
 function App() {
   return (
     <>
-      <h1>You got this 💪</h1>
-      <p>Stuck? Not sure how to proceed?</p>
-      <p>Don't worry - we've all been there. Let's build it together!</p>
+      <Cart />
+      <MealList/>
     </>
   );
 }
