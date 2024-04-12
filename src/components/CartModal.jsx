d572f46947f7c2851aeefc37a27d596e93771b9f
WillamNyunt <williamnyunt@outlook.com>
2024-04-12 15:05:33 +0100

    Cart and Checkout areas added on modal

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -2,13 +2,15 @@ import React, {useContext} from 'react'
 import Modal from './Modal'
 import Cart from './Cart'
 import { ModalContext } from '../context/modal-context'
+import Checkout from './Checkout'
 
 export default function CartModal() {
     const modalCtx = useContext(ModalContext);
 
     return (
         <Modal>
-            {modalCtx.modal.modalOpen && modalCtx.modal.modalType === 'Cart' && <Cart/>}
+            {modalCtx.modal.modalOpen && modalCtx.modal.modalType === 'CART' && <Cart/>}
+            {modalCtx.modal.modalOpen && modalCtx.modal.modalType === 'CHECKOUT' && <Checkout/>}
         </Modal>
     )
 }
